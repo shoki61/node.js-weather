960165df42dcf1cb8ba2f696550d896baca53b10
Sohrat <sohrat6128@gmail.com>
2021-01-20 15:49:05 +0300

    add json parameter

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const apis = require('./env');
 
 const url = `https://api.openweathermap.org/data/2.5/onecall?lat=51.5072&lon=-0.1275&appid=${apis.APIKEY_WEATHER}`;
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data)
+request({ url: url, json: true }, (error, response) => {
+    console.log(`It is currenly ${response.body.current.temp} degrees out. Humidity ${response.body.current.humidity}%`);
 });
