ef3039ff3385672124f05c3c90af7f3135f9d5cd
Sohrat <sohrat6128@gmail.com>
2021-01-20 16:35:08 +0300

    fetch geocode info

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const request = require('request');
 const apis = require('./env');
 
-const url = `https://api.openweathermap.org/data/2.5/onecall?lat=51.5072&lon=-0.1275&appid=${apis.APIKEY_WEATHER}`;
+const url = `https://api.openweathermap.org/data/2.5/onecall?lat=51.5072&lon=-0.1275&appid=${apis.weather}`;
 
 request({ url: url, json: true }, (error, response) => {
     console.log(`It is currenly ${response.body.current.temp} degrees out. Humidity ${response.body.current.humidity}%`);
+});
+
+const geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=london&key=${apis.geocode}`;
+
+request({ url: geocodeURL, json: true }, (error, response) => {
+    console.log(response.body.results[0].geometry.location);
 });
