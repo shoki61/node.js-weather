3f9a8d919ea99de2f637b05600b832c177c51c99
Sohrat <sohrat6128@gmail.com>
2021-01-21 10:47:55 +0300

    Create a weather file under the utils folder, in it create a weather function

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
-const request = require("request");
-const apis = require("./env");
 const geocode = require("./utils/geocode");
-
-const url = `https://api.openweathermap.org/data/2.5/onecall?lat=51.5072&lon=-0.1275&appid=${apis.weather}`;
-
-request({ url: url, json: true }, (error, response) => {
-  if (error) console.log("Unable to connect to weather service!");
-  else if (!response.body.current) console.log("Unable to find location!");
-  else
-    console.log(
-      `It is currenly ${response.body.current.temp} degrees out. Humidity ${response.body.current.humidity}%`
-    );
-});
+const weather = require("./utils/weather");
 
 geocode("london", (error, data) => {
+  console.log("Error", error);
+  console.log("Data", data);
+});
+
+weather(51.5073509, -0.1277583, (error, data) => {
   console.log("Error", error);
   console.log("Data", data);
 });
---
+++ utils/weather.js
@@ -0,0 +1,17 @@
+const request = require("request");
+const apis = require("../env");
+
+const weather = (lat, lng, callback) => {
+  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&appid=${apis.weather}`;
+  request({ url: url, json: true }, (error, response) => {
+    if (error) callback("Unable to connect to weather service!", null);
+    else if (!response.body.current) callback("Unable to find location!", null);
+    else
+      callback(
+        null,
+        `It is currenly ${response.body.current.temp} degrees out. Humidity ${response.body.current.humidity}%`
+      );
+  });
+};
+
+module.exports = weather;
